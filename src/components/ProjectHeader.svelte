215fbc438d9367b1df3e0a7daf2cc016f6c608c9
iamfiro <appturbo102@gmail.com>
2025-02-20 23:05:25 +0900

    feat: enhance ProjectHeader component with GitHub link and TypeScript support

--- src/components/ProjectHeader.svelte
+++ src/components/ProjectHeader.svelte
@@ -1,56 +1,116 @@
-<script>
-  import ArrowLeft from './icons/ArrowLeft.svelte';
-  import HammerWrench from './icons/HammerWrench.svelte';
+<script lang="ts">
+	import ArrowLeft from './icons/ArrowLeft.svelte';
+	import Github from './icons/brand/Github.svelte';
+	import HammerWrench from './icons/HammerWrench.svelte';
+
+	interface ProjectHeaderProps {
+		projectName: string;
+		githubUrl: string;
+	}
+
+	let { projectName = 'Sample Title', githubUrl }: ProjectHeaderProps = $props();
 </script>
 
 <header>
-  <div class="left">
-    <div class="back">
-      <ArrowLeft height={22} width={22} color={'#a0a0a0'} />
-    </div>
-    <HammerWrench height={22} width={22} color={'#919191'} />
-    <h1>PI Visualization</h1>
-  </div>
+	<div class="left">
+		<div class="back">
+			<ArrowLeft height={22} width={22} color={'#a0a0a0'} />
+		</div>
+		<HammerWrench height={22} width={22} color={'#919191'} />
+		<h1>{projectName}</h1>
+	</div>
+	<div>
+		<a class="github" href={githubUrl} target="_blank">
+			<Github height={22} width={22} color={'#ffffff'} />
+			<span>Source Code</span>
+		</a>
+	</div>
 </header>
 
 <style>
-  header {
-    height: 60px;
+	header {
+		/* 드래그 금지 */
+		user-select: none;
 
-    display: flex;
-    align-items: center;
+		height: 70px;
 
-    padding: 0 30px;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
 
-    background-color: white;
-  }
+		padding: 0 30px;
 
-  .left {
-    display: flex;
-    align-items: center;
-    gap: 8px;
+		background-color: white;
+	}
 
-    .back {
-        cursor: pointer;
+	.left {
+		display: flex;
+		align-items: center;
+		gap: 8px;
 
-        width: 35px;
-        height: 35px;
+		.back {
+			cursor: pointer;
 
-        display: flex;
-        justify-content: center;
-        align-items: center;
+			width: 35px;
+			height: 35px;
 
-        border-radius: 8px;
-        border: 2px solid #e1e1e1;
+			display: flex;
+			justify-content: center;
+			align-items: center;
 
-        box-shadow: 0 2px 3px #f2f2f2;
-    }
+			border-radius: 8px;
+			border: 2px solid var(--color-border);
 
-    h1 {
-      font-size: 18px;
-      font-weight: 500;
+			box-shadow: 0 2px 3px #f2f2f2;
 
-      color: #000000;
-    }
-  }
+			margin-right: 10px;
+
+			transition: all 0.1s ease-in-out;
+
+			&:hover {
+				background-color: #f2f2f2;
+			}
+
+			&:active {
+				transform: translateY(2px);
+				box-shadow: 0 1px 2px #f2f2f2;
+			}
+		}
+
+		h1 {
+			font-size: 18px;
+			font-weight: 500;
+
+			color: #000000;
+		}
+	}
+
+	.github {
+		background-color: black;
+		color: white;
+
+		padding: 7px 10px;
+
+		border-radius: 8px;
+
+		display: flex;
+		align-items: center;
+		gap: 5px;
+
+		text-decoration: none;
+
+		transition: all 0.1s ease-in-out;
+
+		&:hover {
+			background-color: #333333;
+		}
+
+		&:active {
+			transform: scale(0.95);
+		}
+
+		span {
+			font-size: 14px;
+		}
+	}
 </style>
--- src/routes/project/+layout.svelte
+++ src/routes/project/+layout.svelte
@@ -1,34 +1,43 @@
 <script>
-
-  import ProjectHeader from "../../components/ProjectHeader.svelte";
-
+	import ProjectHeader from '../../components/ProjectHeader.svelte';
 </script>
 
 <div class="main">
-    <ProjectHeader />
-    <main>
-        <slot />
-    </main>
+	<ProjectHeader projectName={'PI Visualization'} githubUrl={'https://github.com/iamfiro/lab'} />
+	<div class="frame">
+		<main>
+			<slot />
+		</main>
+	</div>
 </div>
 
 <style>
-    .main {
-        width: 100%;
-        height: 100%;
+	.main {
+		width: 100%;
+		height: 100%;
 
-        display: flex;
-        flex-direction: column;
-    }
+		display: flex;
+		flex-direction: column;
+	}
+
+	.frame {
+		width: 100%;
+		height: 100%;
+
+		overflow: hidden;
+
+        padding: 5px 30px 30px 30px;
+	}
 
     main {
         width: 100%;
-        height: calc(100% - 60px);
+        height: 100%;
 
-        display: flex;
-        flex-direction: column;
+        border: 10px;
 
-        overflow: hidden;
+        background-color: white;
 
-        background-color: blue;
+        border: 1px solid #e0e0e0;
+        border-radius: 14px;
     }
 </style>
